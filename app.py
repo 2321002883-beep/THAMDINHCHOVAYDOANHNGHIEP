b48576b4387e62d1f3c425d04140f63d860fb879
2321002883-beep <2321002883@sv.ufm.edu.vn>
2026-07-24 13:32:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 # =========================================================
 
 st.set_page_config(
-    page_title="Thẩm định cho vay doanh nghiệp",
+    page_title="Hệ thống hỗ trợ thẩm định cho vay doanh nghiệp",
     page_icon="🏦",
     layout="wide",
     initial_sidebar_state="expanded"
